e1a742337d3c7540d37621e8d8f538c2aafe7988
Marfien <contact@marfien.dev>
2025-03-08 16:50:14 +0100

    #17 feat: validate deadline of session before

--- src/main/java/com/github/food2gether/sessionservice/service/OrderServiceImpl.java
+++ src/main/java/com/github/food2gether/sessionservice/service/OrderServiceImpl.java
@@ -16,7 +16,9 @@ import jakarta.transaction.Transactional;
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.Response.Status;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @ApplicationScoped
 public class OrderServiceImpl implements OrderService {
@@ -32,13 +34,17 @@ public class OrderServiceImpl implements OrderService {
 
   @Override
   public Order createOrUpdate(Long sessionId, Order.DTO orderDto) {
-    return orderDto.getId() == null ? this.create(sessionId, orderDto) : this.update(orderDto);
+    return orderDto.getId() == null ? this.create(sessionId, orderDto) : this.update(sessionId, orderDto);
   }
 
   @Transactional
   public Order create(Long sessionId, Order.DTO orderDto) {
     Session session = this.sessionRepository.findByIdOptional(sessionId)
         .orElseThrow(() -> new NotFoundException("Session with id " + sessionId + " does not exist"));
+
+    if (session.getDeadline().isBefore(LocalDateTime.now())) {
+      throw new WebApplicationException("Session is already closed", Status.BAD_REQUEST);
+    }
 
     if (orderDto.getId() != null) {
       throw new WebApplicationException("Order id must be null for creating a new order", Status.BAD_REQUEST);
@@ -67,13 +73,26 @@ public class OrderServiceImpl implements OrderService {
   }
 
   @Transactional
-  public Order update(Order.DTO orderDto) {
+  public Order update(Long sessionId, Order.DTO orderDto) {
     if (orderDto.getId() == null) {
       throw new WebApplicationException("Order id must not be null for updating a session", Status.BAD_REQUEST);
     }
 
-    Order existingOrder = this.orderRepository.findByIdOptional(orderDto.getId()).
-        orElse(new Order());
+    Order existingOrder = this.orderRepository.findByIdOptional(orderDto.getId())
+        .orElseThrow(() -> new WebApplicationException(
+            "Order with id " + orderDto.getId() + " does not exist",
+            Status.NOT_FOUND
+        ));
+
+    Session session = existingOrder.getSession();
+
+    if (!Objects.equals(session.getId(), sessionId)) {
+      throw new WebApplicationException("Order does not belong to the session", Status.BAD_REQUEST);
+    }
+
+    if (session.getDeadline().isBefore(LocalDateTime.now())) {
+      throw new WebApplicationException("Session is already closed", Status.BAD_REQUEST);
+    }
 
     if (orderDto.getProfileId() != null)
       existingOrder.setProfile(this.anonymousRepository.findOptional(Profile.class, orderDto.getProfileId())
@@ -114,11 +133,17 @@ public class OrderServiceImpl implements OrderService {
 
   @Override
   public Order getById(Long sessionId, Long orderId) {
-    return this.orderRepository.findByIdOptional(orderId)
+    Order order = this.orderRepository.findByIdOptional(orderId)
         .orElseThrow(() -> new WebApplicationException(
             "Order with id " + orderId + " does not exist",
             Status.NOT_FOUND
         ));
+
+    if (!Objects.equals(order.getSession().getId(), sessionId)) {
+      throw new WebApplicationException("Order does not belong to the session", Status.BAD_REQUEST);
+    }
+
+    return order;
   }
 
   @Override
@@ -126,6 +151,15 @@ public class OrderServiceImpl implements OrderService {
   public Order delete(Long sessionId, Long orderId, String userEmail) {
     Order order = this.orderRepository.findByIdOptional(orderId)
         .orElseThrow(() -> new NotFoundException("Order with id " + orderId + " does not exist"));
+
+    Session session = order.getSession();
+    if (!Objects.equals(session.getId(), sessionId)) {
+      throw new WebApplicationException("Order does not belong to the session", Status.BAD_REQUEST);
+    }
+
+    if (session.getDeadline().isBefore(LocalDateTime.now())) {
+      throw new WebApplicationException("Session is already closed", Status.BAD_REQUEST);
+    }
 
     if (!order.getProfile().getPrimaryEmail().equals(userEmail)) {
       throw new WebApplicationException("Only the user who owns the order can delete it", Status.FORBIDDEN);
